65d402139ebbd7c4f61e26cb5b4de39dc8554097
IraMargolina <79265256+IraMargolina@users.noreply.github.com>
2021-04-03 14:11:51 +0300

    Update LR1.1.py

--- LR1.1.py
+++ LR1.1.py
@@ -1,20 +1,32 @@
-a = 78
-b = 24
-
-def gcd(c, d): 
-    while (c != 0 and d != 0):
+def gcd(c, d):
+    c = abs(c)
+    d = abs(d)
+    while c != 0 and d != 0:
         if c > d:
             c %= d
-        else: d %= c
+        else:
+            d %= c
     return c + d
     
 def lcm(c, d):
+    if c == 0 and d == 0:
+        return 0
+    c = abs(c)
+    d = abs(d)
     m = c * d
-    while (c != 0 and d != 0):
-        if c > d:
-            c %= d
-        else: d %= c
-    return m / (c + d)
+    return m // gcd(c, d)
 
-print ("НОД =", gcd(a, b))
-print ("НОК =", lcm(a, b))
+def main():
+    a = input("Введите число a ")
+    b = input("Введите число b ")
+   
+    try:
+        a = int(a)
+        b = int(b)
+        print("НОД =", gcd(a, b))
+        print("НОК =", lcm(a, b))
+    except:
+        print("Input Error")
+    
+if __name__ == "__main__":
+    main()
